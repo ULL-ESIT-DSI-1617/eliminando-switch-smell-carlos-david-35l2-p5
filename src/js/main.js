519a8b269b558f6b8114484b5fddcf70d793bec2
alu0100851236 <alu0100851236@ull.edu.es>
2017-04-11 18:09:19 +0100

    #2 Eliminada la debilidad del método convertir
    Puesto como método de clase de la clase Medida y el código principal se
    ha encapsulado más

--- src/js/main.js
+++ src/js/main.js
@@ -1,48 +1,8 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
-const regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcCkK])\s*(?:to)?\s*([fFcCkK])/;
 
-function calculate(temperature) {
-  var result = new Temperatura();
-  // var temp = original.value;
-  var temp = temperature;
-  // var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcCkK])\s*(?:to)?\s*([fFcCkK])/;
-
-  var m = temp.match(regexp);
-  if (m) {
-    var t = new Temperatura()
-
-    t.set_valor(parseFloat(m[1]));
-    t.set_tipo_origen(m[2]);
-    t.set_tipo_destino(m[3]);
-
-    if ((t.get_tipo_origen() == 'c' || t.get_tipo_origen() == 'C') && ((t.get_tipo_destino() == 'f' || t.get_tipo_destino() == 'F'))) {
-      result.set_valor(t.convertirC_F());
-      result.set_tipo_destino("F");
-    }
-    else if ((t.get_tipo_origen() == 'c' || t.get_tipo_origen() == 'C') && ((t.get_tipo_destino() == 'k' || t.get_tipo_destino() == 'K'))) {
-      result.set_valor(t.convertirC_K());
-      result.set_tipo_destino("K");
-    }
-    else if ((t.get_tipo_origen() == 'f' || t.get_tipo_origen() == 'F') && ((t.get_tipo_destino() == 'c' || t.get_tipo_destino() == 'C'))) {
-      result.set_valor(t.convertirF_C());
-      result.set_tipo_destino("C");
-    }
-    else if ((t.get_tipo_origen() == 'f' || t.get_tipo_origen() == 'F') && ((t.get_tipo_destino() == 'k' || t.get_tipo_destino() == 'K'))) {
-      result.set_valor(t.convertirF_K());
-      result.set_tipo_destino("K");
-    }
-    else if ((t.get_tipo_origen() == 'k' || t.get_tipo_origen() == 'K') && ((t.get_tipo_destino() == 'c' || t.get_tipo_destino() == 'C'))) {
-      result.set_valor(t.convertirK_C());
-      result.set_tipo_destino("C");
-    }
-    else if ((t.get_tipo_origen() == 'k' || t.get_tipo_origen() == 'K') && ((t.get_tipo_destino() == 'f' || t.get_tipo_destino() == 'F'))) {
-      result.set_valor(t.convertirK_F());
-      result.set_tipo_destino("F");
-    }
-    var muestra = result.mostrar();
-    converted.innerHTML = muestra;
-  }
-  else {
-    converted.innerHTML = "ERROR! Try something like '-4.2C to K' or '35C F' instead";
-  }
+function main() {
+  var medir = new Medida();
+  var temp_convertir = document.getElementById('temp_convert').value;
+  var resultado = document.getElementById('converted');
+  resultado.innerHTML = medir.convertir(temp_convertir);
 }
--- src/js/medida.js
+++ src/js/medida.js
@@ -1,4 +1,5 @@
 "use strict"; // Use ECMAScript 5 strict mode in browsers that support it
+const regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcCkK])\s*(?:to)?\s*([fFcCkK])/;
 
 function Medida (valor, tipo_origen, tipo_destino) {
   this.valor_ = valor;
@@ -13,3 +14,48 @@ Medida.prototype.get_tipo_destino = function(){return this.tipo_destino_;}
 Medida.prototype.set_valor = function(valor){this.valor_ = valor;}
 Medida.prototype.set_tipo_origen = function(tipo_origen){this.tipo_origen_ = tipo_origen;}
 Medida.prototype.set_tipo_destino = function(tipo_destino){this.tipo_destino_ = tipo_destino;}
+
+Medida.prototype.convertir = function(valor) {
+
+    var result = new Temperatura();
+    var _m = valor.match(regexp);
+
+    if (_m) {
+      var t = new Temperatura()
+
+      t.set_valor(parseFloat(_m[1]));
+      t.set_tipo_origen(_m[2]);
+      t.set_tipo_destino(_m[3]);
+
+      if ((t.get_tipo_origen() == 'c' || t.get_tipo_origen() == 'C') && ((t.get_tipo_destino() == 'f' || t.get_tipo_destino() == 'F'))) {
+        result.set_valor(t.convertirC_F());
+        result.set_tipo_destino("F");
+      }
+      else if ((t.get_tipo_origen() == 'c' || t.get_tipo_origen() == 'C') && ((t.get_tipo_destino() == 'k' || t.get_tipo_destino() == 'K'))) {
+        result.set_valor(t.convertirC_K());
+        result.set_tipo_destino("K");
+      }
+      else if ((t.get_tipo_origen() == 'f' || t.get_tipo_origen() == 'F') && ((t.get_tipo_destino() == 'c' || t.get_tipo_destino() == 'C'))) {
+        result.set_valor(t.convertirF_C());
+        result.set_tipo_destino("C");
+      }
+      else if ((t.get_tipo_origen() == 'f' || t.get_tipo_origen() == 'F') && ((t.get_tipo_destino() == 'k' || t.get_tipo_destino() == 'K'))) {
+        result.set_valor(t.convertirF_K());
+        result.set_tipo_destino("K");
+      }
+      else if ((t.get_tipo_origen() == 'k' || t.get_tipo_origen() == 'K') && ((t.get_tipo_destino() == 'c' || t.get_tipo_destino() == 'C'))) {
+        result.set_valor(t.convertirK_C());
+        result.set_tipo_destino("C");
+      }
+      else if ((t.get_tipo_origen() == 'k' || t.get_tipo_origen() == 'K') && ((t.get_tipo_destino() == 'f' || t.get_tipo_destino() == 'F'))) {
+        result.set_valor(t.convertirK_F());
+        result.set_tipo_destino("F");
+      }
+      var muestra = result.mostrar();
+      return muestra;
+    }
+    else {
+      var error = "ERROR! Try something like '-4.2C to K' or '35C F' instead";
+      return error;
+    }
+}
